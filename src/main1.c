7888a2348cf748d1650d2d897a82960a418ca569
jeasika <jesika_18_@hotmail.com>
2021-03-10 02:58:57 +0000

    libraries and main added

---
+++ src/main1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../lib/src"
+
+int main(int argc, char *argv[]) {   
+   
+	float dlrecta;
+	int dhoriz, dvert;
+	
+	dlrecta = dlr( 3, 4, 10, 12 );
+	printf(“Distancia en línea recta: %d\n”, directa);
+	dhoriz = dh( 3, 4, 10, 12 );
+	dvert = dv( 3, 4, 10, 12 );
+      printf(“Distancia horizontal: %d\n”, dhoriz);
+      printf(“Distancia vertical: %d\n”, dvert);
+      
+      return 0;
+}
