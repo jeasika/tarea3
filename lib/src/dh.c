b686c75b5c4c75103113816a162e733513668ca2
jeasika <jesika_18_@hotmail.com>
2021-03-10 00:59:27 +0000

    Files added

---
+++ lib/src/dh.c
@@ -0,0 +1,9 @@
+// Archivo: dh.c
+
+#include "distancias.h"
+
+int dh( int x1, int y1, int x2, int y2 ) {      
+	int r;       
+	r = abs( x1 - x2 );      
+	return r;
+}
---
+++ lib/src/distancias.h
@@ -0,0 +1,11 @@
+// Archivo: distancias.h
+#ifndef _DISTANCIAS_H
+
+#define _DISTANCIAS_Hfloat 
+
+dlr( int, int, int, int );
+int dh( int, int, int, int );
+int dv( int, int, int, int );
+int abs( int );
+
+#endif
