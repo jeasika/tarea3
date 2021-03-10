96c4992f053ddbbca9712c00efcaeacb0a5b1ee2
jeasika <jesika_18_@hotmail.com>
2021-03-10 01:37:52 +0000

    Statics and dynamic files added

--- lib/src/abs.c
+++ lib/src/abs.c
@@ -1,7 +1,8 @@
 // Archivo: abs.c
+
 int abs( int x ) {      
-	if( x < 0 ) 
-		return –x;
+	if ( x < 0 )
+		return -x;
 	else
 		return x;
 }
--- lib/src/distancias.h
+++ lib/src/distancias.h
@@ -1,9 +1,10 @@
 // Archivo: distancias.h
+
 #ifndef _DISTANCIAS_H
 
-#define _DISTANCIAS_Hfloat 
+#define _DISTANCIAS_H
 
-dlr( int, int, int, int );
+float dlr( int, int, int, int );
 int dh( int, int, int, int );
 int dv( int, int, int, int );
 int abs( int );
